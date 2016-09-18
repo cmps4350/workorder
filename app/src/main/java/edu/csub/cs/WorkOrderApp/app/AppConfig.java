843c51f6cea0ddba21f68281174ac8a0dbe6ec1c
123 <you@example.com>
2016-09-18 00:21:56 -0400

    Initial framework

---
+++ app/src/main/java/edu/csub/cs/WorkOrderApp/app/AppConfig.java
@@ -0,0 +1,9 @@
+package edu.csub.cs.WorkOrderApp.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://5.39.77.29/login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://5.39.77.29/login_api/register.php";
+}
