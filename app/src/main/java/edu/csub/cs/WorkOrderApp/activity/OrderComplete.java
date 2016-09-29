f647e264544f83bbcf871c5575d876b9d7723ead
xIDLxSAVAGE <kjenkin3737@yahoo.com>
2016-09-28 18:18:16 -0700

    New branch to work in

---
+++ app/src/main/java/edu/csub/cs/WorkOrderApp/activity/OrderComplete.java
@@ -0,0 +1,15 @@
+package edu.csub.cs.WorkOrderApp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.csub.cs.WorkOrderApp.R;
+
+public class OrderComplete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_complete);
+    }
+}
