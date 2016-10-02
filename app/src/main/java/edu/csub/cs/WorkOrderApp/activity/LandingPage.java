f3d2791e54372e49c0e272f81a17488de044c685
john <jhargreaves1@csub.edu>
2016-10-01 22:49:02 -0700

    Added cogs animation

--- app/src/main/java/edu/csub/cs/WorkOrderApp/activity/LandingPage.java
+++ app/src/main/java/edu/csub/cs/WorkOrderApp/activity/LandingPage.java
@@ -6,8 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import edu.csub.cs.WorkOrderApp.R;
+
+import static edu.csub.cs.WorkOrderApp.R.id.landinglayout;
+import static edu.csub.cs.WorkOrderApp.R.id.linearLayout;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.ImageView;
 
 public class LandingPage extends Activity {
     private static final String TAG = RegisterActivity.class.getSimpleName();
@@ -18,14 +25,34 @@ public class LandingPage extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+        ImageView cogs = (ImageView) findViewById(R.id.cogs);
+        cogs.setBackgroundResource(R.drawable.cogs_animation);
+        AnimationDrawable cogsAnimation = (AnimationDrawable) cogs.getBackground();
+        cogsAnimation.start();
+
         btn_landing = (Button) findViewById(R.id.btn_landing);
+        Thread timerThread = new Thread(){
+            public void run(){
+
+
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(LandingPage.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
 
         btn_landing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LandingPage.this,LoginActivity.class);
                 startActivity(intent);
-                finish();
+
             }
         });
 
